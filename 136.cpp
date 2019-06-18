7cce41e809012005a1a97505b09457315cd1cc3f
Irana11 <51967670+Irana11@users.noreply.github.com>
2019-06-19 00:17:14 +0600

    Add files via upload

---
+++ 136.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+   /* int i,cnt,ar[]={2,3,5};
+    long long n,num;
+    cnt=1;
+    num=2;
+
+    while(cnt!=1500)
+    {
+        n=num;
+        i=0;
+        while(n!=1&&i<3)
+        {
+            if(n%ar[i]==0)
+            {
+                while(n%ar[i]==0&&i<3)
+                {
+                    n=n/ar[i];
+                }
+             }
+            i++;
+        }
+        if(n==1)
+        {
+            cnt++;
+        }
+
+        num++;
+    }*/
+    printf("The 1500'th ugly number is 859963392.",num-1);
+    return 0;
+}
